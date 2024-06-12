53854d209080e83ab92200156dff4d189220790a
oskildo <podolopezospina@gmail.com>
2024-06-12 14:47:54 -0500

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule         } from '@angular/core';
+import { BrowserModule    } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { CounterModule    } from './counter/components/counter/counter.module';
+import { HeroesModule } from './heroes/heroes.module';
+
+import { AppComponent     } from './app.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
